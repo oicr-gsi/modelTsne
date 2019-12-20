7eb3b7b9341cdcd03098448a714be9bd12a7962c
albertojleon <31249960+albertojleon@users.noreply.github.com>
2019-12-20 12:44:08 -0500

    added parameter 'group'

--- R/plotTsne.R
+++ R/plotTsne.R
@@ -1,14 +1,22 @@
-plotTsne<-function(model=NULL,method="plotly",dims1.bw="nrd0", title=NULL){
+plotTsne<-function(model=NULL,method="plotly",dims1.bw="nrd0", title=NULL, group=NULL){
   if(class(model)!="modelTsne"){
     stop("plotTsne: a valid modelTsne object must be provided")
   }
   df<-model$df.tsne
+  if(!is.null(group)){
+    if(length(group)!=nrow(df)){
+      stop("plotTsne: group contains an incorrect number of elements")
+    }else{
+      df$group<-group
+    }
+    
+  }
   dims<-model$dims
   p<-NULL
   if(method=="plotly"){
     #Color palette from the 'scales' package matching ggplot's. The default palette in Plotly produces warnings if no. groups <3 or >8
     col.palette=hue_pal()(length(unique(df$group)))
-
+    
     if(dims==1){
       gg<-ggplot(df, aes(x = tsne.dim1)) +
         stat_density(bw=dims1.bw,aes(group = group, color = group),position="identity",geom="line") +
@@ -44,7 +52,7 @@ plotTsne<-function(model=NULL,method="plotly",dims1.bw="nrd0", title=NULL){
     }
     if(dims==2){
       p<-ggplot(df, aes(x=tsne.dim1, y=tsne.dim2, color=group)) +geom_point() + theme_light() +
-      ggtitle(title) + theme(plot.title = element_text(hjust = 0.5))
+        ggtitle(title) + theme(plot.title = element_text(hjust = 0.5))
     }
     if(dims==3){
       stop("modelTsne::plotTsne: error, ggplot does not support 3d scatterplots")
